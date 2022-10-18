fa8e5e69dee2a8aa8d2590d70dfd3a81f4867327
Christian Beer <christian.beer@chbeer.de>
2022-10-18 17:13:25 +0200

    Switched to Libssh2Prebuild.git

--- Package.swift
+++ Package.swift
@@ -1,22 +1,44 @@
-// swift-tools-version:5.0
-// Managed by ice
+// swift-tools-version:5.6
+// Managed by ice / chbeer
 
 import PackageDescription
 
 let package = Package(
     name: "Shout",
     platforms: [
-        .macOS(.v10_10)
+        .macOS(.v10_10),
+        .iOS(.v12)
     ],
     products: [
-        .library(name: "Shout", targets: ["Shout"]),
+        .library(name: "Shout", targets: ["Shout", "libssh2", "libcrypto"]),
+//        .library(name: "libssh2", targets: ["libssh2"]),
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "1.0.46"),
+        .package(url: "https://github.com/DimaRU/Libssh2Prebuild.git", branch: "1.10.0+OpenSSL_1_1_1o")
     ],
     targets: [
-        .systemLibrary(name: "CSSH", pkgConfig: "libssh2", providers: [.brew(["libssh2","openssl"])]),
-        .target(name: "Shout", dependencies: ["CSSH", "Socket"]),
+        .binaryTarget(
+            name: "libssh2",
+            path: "Sources/Frameworks/libssh2.xcframework"),
+        .binaryTarget(
+            name: "libcrypto",
+            path: "Sources/Frameworks/libcrypto.xcframework"),
+//        .systemLibrary(
+//            name: "CSSH"
+//        ),
+        .target(
+            name: "Shout",
+            dependencies: [
+                .product(name: "CSSH", package: "Libssh2Prebuild"),
+//                "libssh2",
+//                "libcrypto",
+                .product(name: "Socket", package: "BlueSocket")
+            ],
+            linkerSettings: [
+                .linkedLibrary("z")
+            ]
+        ),
         .testTarget(name: "ShoutTests", dependencies: ["Shout"]),
     ]
 )
--- Sources/CSSH/shim.h
+++ Sources/CSSH/shim.h
@@ -1,8 +1,8 @@
 #ifndef __CLIBSSH_SHIM_H__
 #define __CLIBSSH_SHIM_H__
 
-#include <libssh2.h>
-#include <libssh2_sftp.h>
-#include <libssh2_publickey.h>
+#include "../Frameworks/libssh2.xcframework/ios-arm64/Headers/libssh2.h"
+#include "../Frameworks/libssh2.xcframework/ios-arm64/Headers/libssh2_sftp.h"
+#include "../Frameworks/libssh2.xcframework/ios-arm64/Headers/libssh2_publickey.h"
 
 #endif
